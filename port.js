635809f5d0fc2e84f0c0212344e5ae52d5fd6c9b
Deeku728 <dishuchauhansingh@gmail.com>
2024-10-12 05:03:06 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,19 @@
+
+  // Import the functions you need from the SDKs you need
+    import {initializeApp} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+    import {getDatabase,ref,set,get,child} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js";
+    
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+    apiKey: "AIzaSyCVwyQ6nlGzFcO9DMYGWRADLeGjCFxqOl0",
+    authDomain: "e-gram-2da6d.firebaseapp.com",
+    projectId: "e-gram-2da6d",
+    storageBucket: "e-gram-2da6d.appspot.com",
+    messagingSenderId: "406178850869",
+    appId: "1:406178850869:web:cbecc9674508f7909be97c",
+    };
+
+   
